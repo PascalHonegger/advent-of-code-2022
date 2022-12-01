0301c4806706b5552683374ccfeded44c63ff55a
Pascal Honegger <pascal.honegger@bluewin.ch>
2022-12-01 17:18:24 +0100

    Solve day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
+    fun List<String>.splitIntoElves(): List<List<Int>> = buildList {
+        var elf = mutableListOf<Int>()
+        this@splitIntoElves.forEach {
+            if (it.isBlank()) {
+                add(elf)
+                elf = mutableListOf()
+            } else {
+                elf += it.toInt()
+            }
+        }
+        add(elf)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .splitIntoElves()
+            .maxOf { it.sum() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .splitIntoElves()
+            .map { it.sum() }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
